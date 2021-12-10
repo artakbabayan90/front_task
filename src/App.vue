b37238e448e0b050cc7274bdd48295704f89fe1d
artakbabayan90 <arto.90artak@gmail.com>
2021-12-10 17:20:21 +0400

    Create task

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>art</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+
+  methods:{
+    async fetchPosts() {
+      try {
+        const response = await axios.get('https://tests.softmonster.ru/productsData.json', {
+
+        });
+        console.log(response)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+}
+</script>
+
+<style>
+</style>
